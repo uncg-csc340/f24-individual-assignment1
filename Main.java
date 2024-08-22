000697fc79239a69c88848412cc9bd359b27addd
csc340sp24 <csc340sp24@gmail.com>
2024-08-22 19:49:29 -0400

    added  project files

---
+++ Main.java
@@ -0,0 +1,56 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    private static final String FILE_NAME = "about-me.txt";
+
+    public static void main(String[] args) {
+        List<User> classList = new ArrayList<>();
+
+        User sunny = new User("Sunny Ntini",
+                """
+                        I am a lecturer of Computer Science. I have an MS in Comp Sci from UNCG, and a BS in Computer Science
+                        from Indiana University of Pennsylvania. I like (some) superheroes and puzzles.
+                        Just like most basic people, I like hiking. I also like history tidbits (ask me about my favorite
+                        podcasts). I'm currently reading Failure Is An Option by H. Jon Benjamin (Bob's Burgers)
+                        This is the way.
+                        """
+        );
+        classList.add(sunny);
+
+        User nandor = new User("Nandor DeLaurentis",
+                """
+                        I was a very ferocious warrior in the Ottoman Empire, which meant doing a lot of things. A lot
+                        of pillaging. People would say 'hey please don’t pillage me' and I would say 'nooooo, I’m 
+                        pillaging everyone, you included'. I was relentless. They would call me Nandor The Relentless. 
+                        Because I just never relent.
+                        """
+        );
+        classList.add(nandor);
+
+
+        /*
+        ADD YOURSELF TO THE LIST AS SHOWN ABOVE.
+         */
+        writeFile(classList);
+    }
+
+    private static void writeFile(List<User> classList) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(FILE_NAME);
+            for (User user : classList) {
+                writer.write(user.toString());
+            }
+
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
